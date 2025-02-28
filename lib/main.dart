ea225256f18794b74e4d1d30b621ffc0eb357a64
Misbah <misbahmumtaz76@gmail.com>
2025-02-28 00:10:16 -0800

    order

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,5 @@
-// ignore_for_file: prefer_const_constructors, prefer_if_null_operators, prefer_interpolation_to_compose_strings, avoid_print, deprecated_member_use, unnecessary_string_escapes
+//ignore_for_file: prefer_const_constructors, prefer_if_null_operators, prefer_interpolation_to_compose_strings, avoid_print, deprecated_member_use, unnecessary_string_escapes
 
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:get_storage/get_storage.dart';
-// import 'package:hive_flutter/hive_flutter.dart';
-// import 'package:laundry/Api/data_store.dart';
-// import 'package:laundry/helpar/routes_helper.dart';
-// import 'package:laundry/localstring.dart';
-// import 'package:laundry/utils/Custom_widget.dart';
-// import 'package:laundry/utils/cart_item.dart';
-// import 'package:laundry/utils/cartitem_adapter.dart';
-// import 'firebase_options.dart';
-// import 'helpar/get_di.dart' as di;
-//
-// void main() async {
-//   await GetStorage.init();
-//
-//   WidgetsFlutterBinding.ensureInitialized();
-//   // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-//   await Hive.initFlutter();
-//   Hive.registerAdapter(CartItemAdapter());
-//   await Hive.openBox<CartItem>('cart');
-//   await di.init();
-//   initPlatformState();
-//   runApp(
-//     GetMaterialApp(
-//       title: "Soft Laundry",
-//       debugShowCheckedModeBanner: false,
-//       theme: ThemeData(
-//         splashColor: Colors.transparent,
-//         highlightColor: Colors.transparent,
-//         hoverColor: Colors.transparent,
-//         dividerColor: Colors.transparent,
-//         primarySwatch: Colors.blue,
-//         fontFamily: "Gilroy",
-//         useMaterial3: false,
-//         // androidOverscrollIndicator: AndroidOverscrollIndicator.stretch,
-//       ),
-//       translations: LocaleString(),
-//       locale: getData.read("lan2") != null
-//           ? Locale(getData.read("lan2"), getData.read("lan1"))
-//           : Locale('en_US', 'en_US'),
-//       initialRoute: Routes.initial,
-//       getPages: getPages,
-//     ),
-//   );
-// }
-// ignore_for_file: prefer_const_constructors, prefer_if_null_operators, prefer_interpolation_to_compose_strings, avoid_print, deprecated_member_use, unnecessary_string_escapes
-// after configration in to my enviorment main file orginal code...........
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -60,23 +11,22 @@ import 'package:laundry/localstring.dart';
 import 'package:laundry/utils/Custom_widget.dart';
 import 'package:laundry/utils/cart_item.dart';
 import 'package:laundry/utils/cartitem_adapter.dart';
+import 'firebase_options.dart';
 import 'helpar/get_di.dart' as di;
 
 void main() async {
   await GetStorage.init();
 
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Hive.initFlutter();
   Hive.registerAdapter(CartItemAdapter());
   await Hive.openBox<CartItem>('cart');
   await di.init();
   initPlatformState();
-  // // 🔹 Register the controller globally so it's not disposed
-  // Get.put(CurrentOrderController(), permanent: true);
   runApp(
     GetMaterialApp(
-      title: "Fast Laundry",
+      title: "Soft Laundry",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         splashColor: Colors.transparent,
@@ -98,30 +48,4 @@ void main() async {
   );
 }
 
-
-
-
-//
-// import 'package:flutter/material.dart';
-// import 'package:laundry/screen/home_screen.dart';
-// import 'package:laundry/screen/searchScreen.dart';
-//
-//
-// void main() {
-//   runApp(MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Flutter App',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: HomeScreen(), // Set ServicesScreen as the first screen
-//     );
-//   }
-// }
 
--- lib/model/laundryment_search_model.dart
+++ lib/model/laundryment_search_model.dart
@@ -1,15 +1,15 @@
-class Laundry {
-  final String name;
-  final String address;
-  final double distance;
-
-  Laundry({required this.name, required this.address, required this.distance});
-
-  factory Laundry.fromJson(Map<String, dynamic> json) {
-    return Laundry(
-      name: json['laundromat_name'] ?? 'Unknown', // Map `laundromat_name`
-      address: "${json['street_name']}, ${json['city']}, ${json['state']} ${json['zip_code']}", // Concatenate address fields
-      distance: 0.0, // Distance not provided in API, set to default
-    );
-  }
-}
+// class Laundry {
+//   final String name;
+//   final String address;
+//   final double distance;
+//
+//   Laundry({required this.name, required this.address, required this.distance});
+//
+//   factory Laundry.fromJson(Map<String, dynamic> json) {
+//     return Laundry(
+//       name: json['laundromat_name'] ?? 'Unknown', // Map `laundromat_name`
+//       address: "${json['street_name']}, ${json['city']}, ${json['state']} ${json['zip_code']}", // Concatenate address fields
+//       distance: 0.0, // Distance not provided in API, set to default
+//     );
+//   }
+// }
